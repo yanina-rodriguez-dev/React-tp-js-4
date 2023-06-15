ec94a5dfcc65417c92064154551cfaf48704a977
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-06-15 14:58:19 -0300

    funciona por consola

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Container } from "react-bootstrap";
+import Titulo from "./components/Titulo";
+import Formulario from "./components/Formulario";
+import Resultado from "./components/Resultado";
+
+import React from "react";
 
 function App() {
-  
-
   return (
     <>
-     
-    
+      <Container>
+        <Titulo />
+        <Formulario />
+        <Resultado />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
